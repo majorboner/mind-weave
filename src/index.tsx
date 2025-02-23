0b0169e3f368d26d6f96737e62ab9b558c9bec15
Major Boner <khodukin88@gmail.com>
2025-02-24 00:17:39 +0700

    add: prettier, vscode workspace file, upd: gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 
-root.render(<StrictMode><h1>Hello Mr. World</h1></StrictMode>);
+root.render(
+  <StrictMode>
+    <h1>Hello Mr. World</h1>
+  </StrictMode>,
+);
