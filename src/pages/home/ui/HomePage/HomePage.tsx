3f70d0eb64ba6ca6f1a9aaaeb989d99fae1d3b38
Major Boner <khodukin88@gmail.com>
2025-02-24 05:14:01 +0700

    add: home page template

---
+++ src/pages/home/ui/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import cls from './HomePage.module.scss';
+
+export const HomePage = () => {
+  return (
+    <div className={cls.HomePage}>
+      <h1>It&apos;s the home page</h1>
+      <p>end of page</p>
+    </div>
+  );
+};
