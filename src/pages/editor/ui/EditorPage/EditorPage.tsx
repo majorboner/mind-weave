9bc0f065bd84d45c6b2f711b505ab9269b093d6f
Major Boner <khodukin88@gmail.com>
2025-02-24 05:27:51 +0700

    upd: editor page styles

--- src/pages/editor/ui/EditorPage/EditorPage.tsx
+++ src/pages/editor/ui/EditorPage/EditorPage.tsx
@@ -1,13 +1,14 @@
 import { Background, Controls, ReactFlow } from '@xyflow/react';
 import './flow.css';
+import cls from './EditorPage.module.scss';
 
 export const EditorPage = () => {
   return (
-    <>
+    <div className={cls.EditorPage}>
       <ReactFlow width={1000} height={1000}>
         <Background />
         <Controls />
       </ReactFlow>
-    </>
+    </div>
   );
 };
