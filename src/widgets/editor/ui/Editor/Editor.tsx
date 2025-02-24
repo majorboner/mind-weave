317efbb48629fc30d52d4241b2ef0569fd104914
Major Boner <khodukin88@gmail.com>
2025-02-24 11:20:24 +0700

    upd: editor

--- src/widgets/editor/ui/Editor/Editor.tsx
+++ src/widgets/editor/ui/Editor/Editor.tsx
@@ -1,11 +1,9 @@
 import {
   Background,
-  Connection,
   Controls,
-  Edge,
-  EdgeChange,
-  Node,
-  NodeChange,
+  OnConnect,
+  OnEdgesChange,
+  OnNodesChange,
   ReactFlow,
 } from '@xyflow/react';
 import './flow.css';
@@ -13,31 +11,42 @@ import { changeNode, changeEdge, connectNodes } from '../../model/slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { getNodes } from '../../model/selectors/getNodes';
 import { getEdges } from '../../model/selectors/getEdges';
+import cls from './Editor.module.scss';
 
 export const Editor = () => {
   const dispatch = useDispatch();
   const nodes = useSelector(getNodes);
   const edges = useSelector(getEdges);
 
-  const onNodesChange = (changes: NodeChange<Node>[]) =>
+  const onNodesChange: OnNodesChange = (changes) =>
     dispatch(changeNode(changes));
 
-  const onEdgesChange = (changes: EdgeChange<Edge>[]) =>
+  const onEdgesChange: OnEdgesChange = (changes) =>
     dispatch(changeEdge(changes));
 
-  const onConnect = (params: Connection) => dispatch(connectNodes(params));
+  const onConnect: OnConnect = (params) => dispatch(connectNodes(params));
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <Controls />
-    </ReactFlow>
+    <>
+      <div className={cls.debugPanel}>
+        <button onClick={() => console.log(nodes)} className={cls.button}>
+          Print nodes
+        </button>
+        <button onClick={() => console.log(edges)} className={cls.button}>
+          Print edges
+        </button>
+      </div>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </>
   );
 };
