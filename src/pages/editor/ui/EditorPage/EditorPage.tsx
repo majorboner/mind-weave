ace79d94e5ddeb6dc5f84166767c8f336a59c959
Major Boner <khodukin88@gmail.com>
2025-02-24 08:12:43 +0700

    add: template editor

--- src/pages/editor/ui/EditorPage/EditorPage.tsx
+++ src/pages/editor/ui/EditorPage/EditorPage.tsx
@@ -1,11 +1,54 @@
-import { Background, Controls, ReactFlow } from '@xyflow/react';
+import {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  EdgeChange,
+  Node,
+  NodeChange,
+  ReactFlow,
+} from '@xyflow/react';
 import './flow.css';
 import cls from './EditorPage.module.scss';
+import { useCallback, useState } from 'react';
+
+const initialNodes: Node[] = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges: Edge[] = [];
 
 export const EditorPage = () => {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange<Node>[]) =>
+      setNodes((nds: Node[]) => applyNodeChanges(changes, nds)),
+    [],
+  );
+
+  const onEdgesChange = (changes: EdgeChange<Edge>[]) =>
+    setEdges((eds: Edge[]) => applyEdgeChanges(changes, eds));
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
   return (
     <div className={cls.EditorPage}>
-      <ReactFlow width={1000} height={1000}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
